7751c31b006e5d6c2aab1f079499527bb60b20b5
Allen Perl <acperl@xactware.com>
2017-02-04 23:10:04 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,4 @@
+import { run } from './src/index';
+
+console.log('Starting server');
+run();
---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import { MongoClient, Db } from 'mongodb';
+import * as express from 'express';
+import formidable = require('express-formidable');
+import * as path from 'path';
+
+let url = 'mongodb://localhost:27017/emi';
+
+let imageDir = '/data/upload-img';
+
+
+export function run() {
+	let app = express();
+	let db: Db;
+
+	app.use(formidable({
+		encoding: 'utf-8',
+		uploadDir: imageDir,
+		multiples: false,
+		keepExtensions: true
+	}));
+
+	app.use('/upload/images/', express.static(imageDir));
+
+	app.post('/api/card', (req, res) => {
+		var files = req['files'];
+		var fields = req['fields'];
+		var card = JSON.parse(fields.data);
+		card.printed = false;
+		if (files.image) {
+			card.image = path.basename(files.image.path);
+		}
+		db.collection('cards').insert(card);
+		res.writeHead(200, { 'content-type': 'application/json'});
+		res.end(JSON.stringify({
+			message: 'Data Uploaded'
+		}));
+
+	});
+
+	MongoClient.connect(url, (error, database) => {
+		if (error) {
+			console.error(error);
+			return;
+		} else {
+			'Connected to database';
+			db = database;
+			app.listen(3000, () => {
+				'Server is running';
+			})
+		}
+	});
+}
+
